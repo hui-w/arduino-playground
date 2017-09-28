8b73c7301830efc78ec257b37af4cc0254258809
hui-w <hui.wang@autodesk.com>
2017-09-28 16:01:26 +0800

    Car run refactor

--- car-run/car-run.c
+++ car-run/car-run.c
@@ -2,138 +2,64 @@
  * The basic control to the car:
  * Go forward, go backward, turn left and turn right
  */
-int leftMotorBackwardPin = 9; // Left motor backward (IN1)
-int leftMotorForwardPin = 5; // Left motor forward (IN2)
-
-int rightMotorForwardPin = 6; // Right motor forward (IN3)
-int rightMotorBackwardPin = 10; // Right motor backward (IN4)
 
 /*
- * Go forward
+ * 9 - Left motor backward (IN1)
+ * 5 - Left motor forward (IN2)
+ * 6 - Right motor forward (IN3)
+ * 10 - Right motor backward (IN4)
  */
-void goForward(int time) {
-  // Right motor goes forward
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
+int pins[2][2] = {
+  { 5, 9 },
+  { 6, 10 }
+};
 
-  /*
-   * Writes an analog value (PWM wave) to a pin drive a motor at various speeds.
-   * PWM: Pulse Width Modulation (0~255)
-   * https://www.arduino.cc/en/Tutorial/PWM
-   */
-  analogWrite(rightMotorForwardPin, 200);
-  analogWrite(rightMotorBackwardPin, 0);
+/*
+ * Drive the motor
+ *
+ * motorIndex  >>                0: left;    1: right
+ * driveMode   >>  -1: backward; 0: break;   1: forward
+ */
+void driveMotor(int motorIndex, int driveMode) {
+  int pinForward = pins[motorIndex][0];
+  int pinBackward = pins[motorIndex][1];
 
-  // Left motor goes forward
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorForwardPin, 200);
-  analogWrite(leftMotorBackwardPin, 0);
-
-  delay(time * 1000);
+  if (driveMode == -1) {
+    // Backward
+    digitalWrite(pinForward, LOW);
+    digitalWrite(pinBackward, HIGH);
+    /*
+     * Writes an analog value (PWM wave) to a pin drive a motor at various speeds.
+     * PWM: Pulse Width Modulation (0~255)
+     * https://www.arduino.cc/en/Tutorial/PWM
+     */
+    analogWrite(pinForward, 0);
+    analogWrite(pinBackward, 200);
+  } else if (driveMode == 0) {
+    // Break
+    digitalWrite(pinForward, LOW);
+    digitalWrite(pinBackward, LOW);
+    analogWrite(pinForward, 0);
+    analogWrite(pinBackward, 0);
+  } else if (driveMode == 1) {
+    // Forward
+    digitalWrite(pinForward, HIGH);
+    digitalWrite(pinBackward, LOW);
+    analogWrite(pinForward, 200);
+    analogWrite(pinBackward, 0);
+  }
 }
 
 /*
- * Break
+ * Drive the car
+ *
+ * leftMode   >>  -1: backward; 0: break;   1: forward
+ * rightMode  >>  -1: backward; 0: break;   1: forward
+ * time       >>  seconds
  */
-void brake(int time) {
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-
-  delay(time * 1000);
-}
-
-/*
- * Turn left
- */
-void turnLeft(int time) {
-  // Right motor goes forward
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  analogWrite(rightMotorForwardPin, 200);
-  analogWrite(rightMotorBackwardPin, 0);
-
-  // Left motor breaks
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorForwardPin, 0);
-  analogWrite(leftMotorBackwardPin, 0);
-
-  delay(time * 1000);
-}
-
-/*
- * Spn left
- */
-void spinLeft(int time) {
-  // Right motor goes forward
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  analogWrite(rightMotorForwardPin, 200);
-  analogWrite(rightMotorBackwardPin, 0);
-
-  // Left motor goes backward
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, HIGH);
-  analogWrite(leftMotorForwardPin, 0);
-  analogWrite(leftMotorBackwardPin, 200);
-  delay(time * 1000);
-}
-
-/*
- * Turn right
- */
-void turnRight(int time) {
-  // Right motor breaks
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  analogWrite(rightMotorForwardPin, 0);
-  analogWrite(rightMotorBackwardPin, 0);
-
-  // Left motor goes forward
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorForwardPin, 200);
-  analogWrite(leftMotorBackwardPin, 0);
-
-  delay(time * 1000);
-}
-
-/*
- * Spin right
- */
-void spinRight(int time) {
-  // Right motor goes backward
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, HIGH);
-  analogWrite(rightMotorForwardPin, 0);
-  analogWrite(rightMotorBackwardPin, 200);
-
-  // Left motor goes forward
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  analogWrite(leftMotorForwardPin, 200);
-  analogWrite(leftMotorBackwardPin, 0);
-  delay(time * 1000);
-}
-
-/*
- * Go backward
- */
-void goBack(int time) {
-  // Right motor goes backward
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, HIGH);
-  analogWrite(rightMotorForwardPin, 0);
-  analogWrite(rightMotorBackwardPin, 150);
-  digitalWrite(leftMotorForwardPin, LOW);
-
-  // Left motor goes backward
-  digitalWrite(leftMotorBackwardPin, HIGH);
-  analogWrite(leftMotorForwardPin, 0);
-  analogWrite(leftMotorBackwardPin, 150);
+void driveCar(int leftMode, int rightMode, int time) {
+  driveMotor(0, leftMode);
+  driveMotor(0, rightMode);
   delay(time * 1000);
 }
 
@@ -143,29 +69,30 @@ void setup() {
    * On UNO, digitalWrite() works on pins 3, 5, 6, 9, 10, and 11.
    * https://www.arduino.cc/en/Reference/analogWrite
    */
-  pinMode(leftMotorForwardPin, OUTPUT); // PIN 5
-  pinMode(leftMotorBackwardPin, OUTPUT); // PIN 9
-  pinMode(rightMotorForwardPin, OUTPUT); // PIN 6
-  pinMode(rightMotorBackwardPin, OUTPUT); // PIN 10
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      pinMode(pins[i][j], OUTPUT);
+    }
+  }
 }
 
 void loop() {
   // Freeze for 2 seconds before starting
   delay(2000);
 
-  goBack(1);
-  brake(0.5);
+  // Go forward
+  driveCar(-1, -1, 0.5);
 
-  goForward(1);
-  brake(0.5);
+  // Go forward
+  driveCar(1, 1, 0.5);
 
-  turnLeft(1);
-  turnRight(1);
-  brake(0.5);
+  // Turn left; Turn right; Break
+  driveCar(0, 1, 1);
+  driveCar(1, 0, 1);
+  driveCar(0, 0, 1);
 
-  spinRight(2);
-  spinLeft(2);
-  brake(0.5);
-
-  brake(0.5);
+  // Spin right; Spin left; Break
+  driveCar(1, -1, 2);
+  driveCar(-1, 1, 2);
+  driveCar(0, 0, 1);
 }
